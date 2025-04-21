aa1331b9ad6a51a1b033f059e239edaa600f2eb8
gemmaqu <gqu@smith.edu>
2025-04-21 19:18:56 -0400

    gemmacode

--- Friend.java
+++ Friend.java
@@ -1,3 +1,25 @@
-public class Friend {
+public class Friend extends Person{
+
+    //attributes
+   private boolean isFriend;
+
+   //constructor
+   public Friend(int age, String name, int relationshipValue, boolean isAlive){
+    super(age, name, relationshipValue, isAlive);
+    this.isFriend = true;
+   }
+
+   public boolean getisFriends(Player player){
+    System.out.println(Player.getname(player)+" and "+ getname(this)+ " are friends");
+    return this.isFriend;
+   }
+
+   public boolean breakupwfriends(Player player){
+    System.out.println(Player.getname(player)+" and "+ getname(this)+ " are not friends anymore.");
+    return this.isFriend == false;
+   }
+
+
+
 
 }
