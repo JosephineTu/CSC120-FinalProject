ce0f8717a1b804145aba417b26e80670315af7a8
JosephineTu <jtu@smith.edu>
2025-04-22 10:35:39 -0400

    made changes to RunGame

--- Job.java
+++ Job.java
@@ -3,6 +3,8 @@ public class Job {
     private DEGREE leastDegreeRequired;
     private int annualIncome;
     public Job(String name,DEGREE leastDegreeRequired,int annualIncome){
-        this.name
+        this.name=name;
+        this.leastDegreeRequired=leastDegreeRequired;
+        this.annualIncome=annualIncome;
     }
 }
