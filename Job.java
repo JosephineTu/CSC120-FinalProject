35f7a1cd812bc147c664a219ca435f398772448d
gemmaqu <gqu@smith.edu>
2025-04-29 10:23:03 -0400

    javadoc

--- Job.java
+++ Job.java
@@ -1,7 +1,16 @@
 public class Job {
+    
+    //attributes
     public String name;
     private DEGREE leastDegreeRequired;
     private int annualIncome;
+
+    /**
+     * Constructor
+     * @param name
+     * @param leastDegreeRequired
+     * @param annualIncome
+     */
     public Job(String name,DEGREE leastDegreeRequired,int annualIncome){
         this.name=name;
         this.leastDegreeRequired=leastDegreeRequired;
--- Player.java
+++ Player.java
@@ -1,9 +1,25 @@
-import java.util.ArrayList;
+import java.util.ArrayList;// import the package we need 
+
 public class Player extends Person{
-    public Health health;
-    public Intelligence intelligence;
-    public Money money;
-    public ArrayList<Person> people;
+// this is a child class of person, besides all the attributes from the parent class, it has other attributes that we need to use in the game.
+
+    //attributes
+    public Health health; // the health of the player
+    public Intelligence intelligence; // the interlligence of the player
+    public Money money; // the amount of money the player has
+    public ArrayList<Person> people; // the list of friend and family member of the player
+
+    /**
+     * constructor
+     * @param age
+     * @param name
+     * @param relationshipValue
+     * @param isAlive
+     * @param health
+     * @param intelligence
+     * @param money
+     * @param people
+     */
     public Player(int age, String name, int relationshipValue, boolean isAlive,Health health,Intelligence intelligence,Money money,ArrayList<Person>people){
         super(age, name, relationshipValue, isAlive);
         this.health = health;
@@ -11,6 +27,10 @@ public class Player extends Person{
         this.money = money;
         this.people=people;
     }
+
+    /**
+     * the intelligence value of the player could be increase if reading books
+     */
     public void readBook(){
         if(this.intelligence.value<98){
             this.intelligence.value+=3;
